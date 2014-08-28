94910b064f1a88d031c4e38a42a8c9eca455421f
rudin23 <rudin_lopez@hotmail.com>
2014-08-28 15:43:41 -0600

    listado de clases, primeras clases
    agregue los primeros listados de posibles clases y tambien las primeras
    clases o primeros intentos de clases en una carpeta llamada Love Letter

---
+++ Love Letter/Menu.java
@@ -0,0 +1,16 @@
+public class Menu{
+   public static void main(String args[]){
+      mensaje();
+	  
+   }//Fin del método principla (main)
+   
+   public static void mensaje(){
+      System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+      System.out.println("-= Bienvenido a Love Letter=-");
+	  System.out.println("(1) Jugar Una Partida");
+	  System.out.println("(2) Seleccionar el Número de Tokens a Juagar");
+	  System.out.println("(3) Mostrar las Reglas del Juego");
+	  System.out.println("(4) Salir");
+	  System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+   }//Fin del método mensaje
+}//FIn de la clase Menu
